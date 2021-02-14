76768f07e254a72053b11d5988634bb1e26c5650
irecolo94 <ireblue@virgilio.it>
2021-02-14 19:12:05 +0100

    feat: webpack set up to bundle feat: css duplicate to scss to write sass

--- evaluate-news-nlp/src/client/js/formHandler.js
+++ evaluate-news-nlp/src/client/js/formHandler.js
@@ -3,14 +3,20 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
+    Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+    fetch('http://localhost:8081/test')
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
+    }),
+    fetch('https://api.openweathermap.org/data/2.5/weather?zip=90210&appid=6e30485752cf2a3013a66cca6d1829f6&units=metric')
+    .then(res => res.json())
+    .then(function(data) {
+        document.getElementById('results').innerHTML += data.main.temp
     })
+
 }
 
 export { handleSubmit }
